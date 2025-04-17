8460572945f5baab40ed6b672174764b29f796ab
Umesh Gautam <gautamsandesh15@gmail.com>
2025-04-17 10:20:45 +0545

    adding notification icon in navbar

--- frontend_rentify/src/components/Navbar.jsx
+++ frontend_rentify/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import logo from '/logo.png';
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaBell } from "react-icons/fa";
 import Modal from "./Modal";
 import { AuthContext } from "../Contexts/AuthProvider";
 import Profile from "./Profile";
@@ -98,10 +98,8 @@ const Navbar = () => {
         <div className="navbar-end flex items-center space-x-5">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-5 flex items-center justify-center">
             <div className="indicator">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-              </svg>
-              <span className="badge badge-sm indicator-item">5</span>
+              <FaBell className="h-5 w-5" />
+              <span className="badge badge-sm indicator-item">3</span>
             </div>
           </div>
 
